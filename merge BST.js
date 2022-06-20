ea9033164a96683cf2053b19e27e18068a723ee1
Ashish K <ashishskabade@gmail.com>
2022-06-20 19:20:28 +0530

    mergeBST

--- merge BST.js
+++ merge BST.js
@@ -1,57 +1,74 @@
-class Node{
-    constructor(key){
-        this.left = null;
-        this.right = null;
-        this.val=key;
+// class Node{
+//     constructor(key){
+//         this.left = null;
+//         this.right = null;
+//         this.val=key;
+//     }
+// }
+// function insert(root,key){
+//     if(root===null){
+//         return new Node(key);
+//     }
+//     else{
+//         if(root.val===key) return root;
+//         else if(root.val<key) root.right=insert(root.right,key);
+//         else root.left = insert(root.left,key);
+//     }
+//     return root;
+// }
+// r =new Node(50);
+// insert(r,20);
+// insert(r,30);
+// insert(r,15);
+// insert(r,80);
+// insert(r,75);
+// insert(r,95);
+// r1 =new Node(5);
+// insert(r1,3);
+// insert(r1,2);
+// insert(r1,4);
+// insert(r1,100);
+// insert(r1,90);
+// insert(r1,110);
+// console.log(r1);
+// function mergeBST(root1,root2){
+//     var list=[];
+//     function inorder(root){
+//         if(root){
+//             inorder(root.left);
+//             list.push(root.val);
+//             inorder(root.right);
+//         }
+//     }
+//     inorder(root1)
+//     inorder(root2)
+//     list.sort((a,b)=>{return a-b});
+//     console.log(list)
+//     function buildTree(list,start,end){
+//         if(start>end) return null;
+//         var mid=parseInt((start+end)/2);
+//         var node=new Node(list[mid]);
+//         node.left=buildTree(list,start,mid-1);
+//         node.right=buildTree(list,mid+1,end);
+//         return node;
+//     }
+//     return buildTree(list,0,list.length-1);
+// }
+// (mergeBST(r,r1));
+
+async function fun1(){
+    console.log('a');
+    console.log('b');
+    const c= new Promise((res,err)=>{setTimeout(() => {
+        res('c');
+    }, 1000)});
+    console.log(await c);
+    const d= new Promise((res,err)=>{setTimeout(() => {
+        res('d');
+    }, 0)});
+    console.log(await d);
+    console.log('e');
     }
-}
-function insert(root,key){
-    if(root===null){
-        return new Node(key);
-    }
-    else{
-        if(root.val===key) return root;
-        else if(root.val<key) root.right=insert(root.right,key);
-        else root.left = insert(root.left,key);
-    }
-    return root;
-}
-r =new Node(50);
-insert(r,20);
-insert(r,30);
-insert(r,15);
-insert(r,80);
-insert(r,75);
-insert(r,95);
-r1 =new Node(5);
-insert(r1,3);
-insert(r1,2);
-insert(r1,4);
-insert(r1,100);
-insert(r1,90);
-insert(r1,110);
-console.log(r1);
-function mergeBST(root1,root2){
-    var list=[];
-    function inorder(root){
-        if(root){
-            inorder(root.left);
-            list.push(root.val);
-            inorder(root.right);
-        }
-    }
-    inorder(root1)
-    inorder(root2)
-    list.sort((a,b)=>{return a-b});
-    console.log(list)
-    function buildTree(list,start,end){
-        if(start>end) return null;
-        var mid=parseInt((start+end)/2);
-        var node=new Node(list[mid]);
-        node.left=buildTree(list,start,mid-1);
-        node.right=buildTree(list,mid+1,end);
-        return node;
-    }
-    return buildTree(list,0,list.length-1);
-}
-(mergeBST(r,r1));
+fun1();
+
+    
